dc62bc679d54a82ed066b32cd30390ab47beb991
HomePC <mordovia1989@gmail.com>
2023-09-03 22:05:54 +0300

    chengcod

--- Program.cs
+++ Program.cs
@@ -20,3 +20,15 @@ namespace cyb28
         }
     }
 }
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Привіт, я навчаюся у школі Hillel!");
+            Console.ReadKey();
+        }
+    }
+}

b27ac96cdab595718df537acbc34cc1f2b63373c
HomePC <mordovia1989@gmail.com>
2023-09-03 22:03:47 +0300

    first rep

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System; //Smorg Maxim Hillel
+
+namespace cyb28
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Insert namber: ");
+            long n = long.Parse(Console.ReadLine()), rez = 0, k = 0;
+
+            while ((k = n % 10) != 0 || n / 10 != 0)
+            {
+                rez = rez * 10 + k;
+                n /= 10;
+            }
+
+            Console.WriteLine("Autpoot: {0}", rez);
+            Console.ReadKey();
+        }
+    }
+}
